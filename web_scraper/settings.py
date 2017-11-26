dd9fbc2699a2a2bdc3e6ab8c46a13cbb03ca1ee8
Simon <simon.moniereabes@gmail.com>
2017-11-25 20:46:16 -0500

    Modified the starting urls, and added some error handling

--- web_scraper/settings.py
+++ web_scraper/settings.py
@@ -23,6 +23,7 @@ ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by web_scraper (default: 16)
 #CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
